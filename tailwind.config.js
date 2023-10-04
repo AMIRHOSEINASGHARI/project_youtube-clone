a817e9ea75cd0b7cac4085d4fcd6ce16e7b579cc
AMIRHOSEIN <amirhoseinasghari2000@gmail.com>
2023-10-04 09:12:58 +0330

    Tailwind configuration

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './src/pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/components/**/*.{js,ts,jsx,tsx,mdx}',
-    './src/app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      colors: {
+        "dark-1": "#0F0F0F",
+        "gray-1": "#65748E",
+        "gray-2": "#F2F2F2",
+        "gray-3": "#CCCCCC",
       },
     },
   },
   plugins: [],
-}
+};
